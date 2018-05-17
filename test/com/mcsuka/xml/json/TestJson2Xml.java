bd9695c42f966783cbe1a1728d5b071d5e3f57c3
Miklos <miklos_csuka@yahoo.com>
2018-05-17 18:08:15 +0200

    improved array and attribute handling

--- test/com/mcsuka/xml/json/TestJson2Xml.java
+++ test/com/mcsuka/xml/json/TestJson2Xml.java
@@ -13,10 +13,10 @@ import com.mcsuka.xml.testtools.GenericTools;
 
 public class TestJson2Xml {
 
-  private static String json2xml(String rootElem, String jsonFileName) throws Exception {
+  private static String json2xml(String rootElem, String jsonFileName, String nameSpaceUri) throws Exception {
       String jsonFile = GenericTools.getResourceFile(jsonFileName);
       Json2Xml json2xml = new Json2Xml();
-      Document doc = json2xml.translate(jsonFile, rootElem, null);
+      Document doc = json2xml.translate(jsonFile, rootElem, nameSpaceUri);
       return XmlTools.renderDOM(doc);
   }
 
@@ -32,14 +32,21 @@ public class TestJson2Xml {
 
   @Test
   public void testJson2XmlNoxsd1() throws Exception {
-    String actual = json2xml("root2", "testdata/input/Json2XmlNoxsd1.json");
+    String actual = json2xml("root2", "testdata/input/Json2XmlNoxsd1.json", null);
     String expected = GenericTools.getResourceFile("testdata/output/Json2XmlNoxsd1.xml");
     GenericTools.assertEquals(expected, actual);
   }
 
   @Test
+  public void testJson2XmlNoxsd2() throws Exception {
+    String actual = json2xml("root2", "testdata/input/Json2XmlNoxsd2.json", "urn:test");
+    String expected = GenericTools.getResourceFile("testdata/output/Json2XmlNoxsd2.xml");
+    GenericTools.assertEquals(expected, actual);
+  }
+
+  @Test
   public void testJson2XmlStrangeCharacters() throws Exception {
-    String actual = json2xml("root2", "testdata/input/Json2XmlStrangeCharacters.json");
+    String actual = json2xml("root2", "testdata/input/Json2XmlStrangeCharacters.json", null);
     String expected = GenericTools.getResourceFile("testdata/output/Json2XmlStrangeCharacters.xml");
     GenericTools.assertEquals(expected, actual);
   }
@@ -65,10 +72,88 @@ public class TestJson2Xml {
     GenericTools.assertEquals(expected, actual);
   }
 
+
+  @Test
+  public void testJson2XmlXsd4() throws Exception {
+    String actual = json2xmlNs("testdata/input/Simple.xsd", "root2", "testdata/input/Json2XmlXsd4.json");
+    String expected = GenericTools.getResourceFile("testdata/output/Json2XmlXsd4.xml");
+    GenericTools.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testJson2XmlXsd5() throws Exception {
+    String actual = json2xmlNs("testdata/input/Simple.xsd", "root4", "testdata/input/Json2XmlXsd5.json");
+    String expected = GenericTools.getResourceFile("testdata/output/Json2XmlXsd5.xml");
+    GenericTools.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testJson2XmlXsd6() throws Exception {
+    String actual = json2xmlNs("testdata/input/Simple.xsd", "root4", "testdata/input/Json2XmlXsd6.json");
+    String expected = GenericTools.getResourceFile("testdata/output/Json2XmlXsd6.xml");
+    GenericTools.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testJson2XmlXsd7() throws Exception {
+    String actual = json2xmlNs("testdata/input/Simple.xsd", "root5", "testdata/input/Json2XmlXsd7.json");
+    String expected = GenericTools.getResourceFile("testdata/output/Json2XmlXsd7.xml");
+    GenericTools.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testJson2XmlXsd8() throws Exception {
+    String actual = json2xmlNs("testdata/input/Simple.xsd", "root5", "testdata/input/Json2XmlXsd8.json");
+    String expected = GenericTools.getResourceFile("testdata/output/Json2XmlXsd8.xml");
+    GenericTools.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testJson2XmlXsd9() throws Exception {
+    String actual = json2xmlNs("testdata/input/Simple.xsd", "root5", "testdata/input/Json2XmlXsd9.json");
+    String expected = GenericTools.getResourceFile("testdata/output/Json2XmlXsd9.xml");
+    GenericTools.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testJson2XmlXsd10() throws Exception {
+    String actual = json2xmlNs("testdata/input/Simple.xsd", "root6", "testdata/input/Json2XmlXsd10.json");
+    String expected = GenericTools.getResourceFile("testdata/output/Json2XmlXsd10.xml");
+    GenericTools.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testJson2XmlXsd11() throws Exception {
+    String actual = json2xmlNs("testdata/input/Simple.xsd", "root6", "testdata/input/Json2XmlXsd11.json");
+    String expected = GenericTools.getResourceFile("testdata/output/Json2XmlXsd11.xml");
+    GenericTools.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testJson2XmlXsd12() throws Exception {
+    String actual = json2xmlNs("testdata/input/Simple.xsd", "root6", "testdata/input/Json2XmlXsd12.json");
+    String expected = GenericTools.getResourceFile("testdata/output/Json2XmlXsd12.xml");
+    GenericTools.assertEquals(expected, actual);
+  }
+
   @Test
   public void testJson2XmlNoxsdArray() throws Exception {
-    String actual = json2xml("root", "testdata/input/Json2XmlNoxsdArray.json");
+    String actual = json2xml("root", "testdata/input/Json2XmlNoxsdArray.json", null);
     String expected = GenericTools.getResourceFile("testdata/output/Json2XmlNoxsdArray.xml");
+    GenericTools.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testJson2XmlNoxsdArray2() throws Exception {
+    String actual = json2xml("root", "testdata/input/Json2XmlNoxsdArray2.json", null);
+    String expected = GenericTools.getResourceFile("testdata/output/Json2XmlNoxsdArray2.xml");
+    GenericTools.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testJson2XmlNoxsdArray3() throws Exception {
+    String actual = json2xml("root", "testdata/input/Json2XmlNoxsdArray3.json", null);
+    String expected = GenericTools.getResourceFile("testdata/output/Json2XmlNoxsdArray3.xml");
     GenericTools.assertEquals(expected, actual);
   }
 
@@ -76,6 +161,27 @@ public class TestJson2Xml {
   public void testJson2XmlXsdArray() throws Exception {
     String actual = json2xmlNs("testdata/input/Array.xsd", "root", "testdata/input/Json2XmlXsdArray.json");
     String expected = GenericTools.getResourceFile("testdata/output/Json2XmlXsdArray.xml");
+    GenericTools.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testJson2XmlXsdArray2() throws Exception {
+    String actual = json2xmlNs("testdata/input/Array2.xsd", "root", "testdata/input/Json2XmlXsdArray2.json");
+    String expected = GenericTools.getResourceFile("testdata/output/Json2XmlXsdArray2.xml");
+    GenericTools.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testJson2XmlXsdArray3() throws Exception {
+    String actual = json2xmlNs("testdata/input/Array2.xsd", "root", "testdata/input/Json2XmlXsdArray3.json");
+    String expected = GenericTools.getResourceFile("testdata/output/Json2XmlXsdArray3.xml");
+    GenericTools.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testJson2XmlXsdNoArray() throws Exception {
+    String actual = json2xmlNs("testdata/input/NoArray.xsd", "root", "testdata/input/Json2XmlXsdNoArray.json");
+    String expected = GenericTools.getResourceFile("testdata/output/Json2XmlXsdNoArray.xml");
     GenericTools.assertEquals(expected, actual);
   }
 
@@ -88,7 +194,7 @@ public class TestJson2Xml {
 
   @Test
   public void testJson2XmlNoxsdScalar() throws Exception {
-    String actual = json2xml("root", "testdata/input/Json2XmlNoxsdScalar.json");
+    String actual = json2xml("root", "testdata/input/Json2XmlNoxsdScalar.json", null);
     String expected = GenericTools.getResourceFile("testdata/output/Json2XmlNoxsdScalar.xml");
     GenericTools.assertEquals(expected, actual);
   }
