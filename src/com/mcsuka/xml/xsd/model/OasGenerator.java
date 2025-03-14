cb7e92ed0cb8932b5a3903af2e4f0df7c3a56e52
miklos.csuka <miklos.csuka@odido.nl>
2025-03-14 20:28:29 +0100

    oas generator save for the night

--- src/com/mcsuka/xml/xsd/model/OasGenerator.java
+++ src/com/mcsuka/xml/xsd/model/OasGenerator.java
@@ -1,6 +1,11 @@
 package com.mcsuka.xml.xsd.model;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class OasGenerator {
 
@@ -8,4 +13,33 @@ public class OasGenerator {
 
         return null; // ToDo
     }
+
+//    private static JsonObject renderSchema(JsonArray input) {
+//        JsonObject schema = new JsonObject();
+//        schema.addProperty("type", "array");
+//        if (!input.isEmpty() && input.get(0) instanceof JsonObject) {
+//            schema.add("items", renderSchema((JsonObject) input.get(0)));
+//        } else {
+//            JsonObject type = new JsonObject();
+//            type.addProperty("type", "string");
+//            schema.add("items", type);
+//        }
+//        return schema;
+//    }
+
+
+
+    public static JsonObject oasService(SoapRestServiceDefinition serviceDef) {
+        JsonObject service = new JsonObject();
+        service.addProperty("summary", serviceDef.description());
+        JsonArray parameters = new JsonArray();
+        serviceDef.requestParameters().forEach(paramDef -> {
+            parameters.add(paramDef.asOasServiceParam());
+        });
+
+
+        return service;
+    }
+
+
 }
--- src/com/mcsuka/xml/xsd/model/SoapRestServiceDefinition.java
+++ src/com/mcsuka/xml/xsd/model/SoapRestServiceDefinition.java
@@ -2,10 +2,14 @@ package com.mcsuka.xml.xsd.model;
 
 import com.mcsuka.xml.xsd.tools.WsdlDocumentSource;
 
+import java.util.List;
+
 public record SoapRestServiceDefinition(
     String restPath,
     String restMethod,
+    List<RequestParameter> requestParameters,
     WsdlDocumentSource wsdlSource,
-    WsdlDocumentSource.SoapOperation operation
+    WsdlDocumentSource.SoapOperation operation,
+    String description
 ) {
 }
